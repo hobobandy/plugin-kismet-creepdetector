3906de27639950329ea61689f085c42d01ab332d
hobobandy <30026704+hobobandy@users.noreply.github.com>
2023-01-22 11:01:24 -0500

    Integrate settings into Web UI
    Allows user to customize threshold distance and unit, and toggle showing only creeps.

--- httpd/js/creepdetector.js
+++ httpd/js/creepdetector.js
@@ -15,6 +15,10 @@ const REFRESH_INTERVAL = 10000;
 const DISTANCE_UNIT = 'mile';
 const DISTANCE_THRESHOLD = 0.2;
 
+var creep_thresh_dist = kismet.getStorage('kismet.creepdetector.thresh_dist', DISTANCE_THRESHOLD);
+var creep_thresh_unit = kismet.getStorage('kismet.creepdetector.thresh_unit', DISTANCE_UNIT);
+var creep_show_only_creeps = kismet.getStorage('kismet.creepdetector.show_only_creeps');
+
 kismet_ui_tabpane.AddTab({
     id: 'creepdetector',
     tabTitle: 'CreepDetector',
@@ -33,7 +37,7 @@ kismet_ui_tabpane.AddTab({
                             <th>Last RSSI</th>
                             <th>Lat</th>
                             <th>Lon</th>
-                            <th>Haversine</th>
+                            <th>Haversine (`+creep_thresh_unit+`)</th>
                         </tr>
                     </thead>
                     <tfoot>
@@ -45,7 +49,7 @@ kismet_ui_tabpane.AddTab({
                             <th>Last RSSI</th>
                             <th>Lat</th>
                             <th>Lon</th>
-                            <th>Haversine</th>
+                            <th>Haversine (`+creep_thresh_unit+`)</th>
                         </tr>
                     </tfoot>
                 </table>
@@ -98,11 +102,14 @@ kismet_ui_tabpane.AddTab({
                         
                         const d_haversine = haversine(d['min_loc']['kismet.common.location.geopoint'], 
                                                       d['max_loc']['kismet.common.location.geopoint'], 
-                                                      {format: "[lon,lat]", unit: DISTANCE_UNIT});
+                                                      {format: "[lon,lat]", unit: creep_thresh_unit});
                         
                         // Skip devices not creeping
-                        //if (d_haversine < DISTANCE_THRESHOLD)
-                        //    continue
+                        if (creep_show_only_creeps) {
+                          if (d_haversine < creep_thresh_dist) {
+                            continue
+                          }
+                        }
                         
                         const d_row = [
                             d['kismet.device.base.name'],
@@ -133,3 +140,184 @@ kismet_ui_tabpane.AddTab({
         }); // end of document.ready
     }, // end of createCallback
 }); // end of AddTab
+
+kismet_ui_settings.AddSettingsPane({
+  id: 'plugin_creepdetector',
+  listTitle: 'CreepDetector',
+  create: function(elem) {
+      elem.append(
+          $('<form>', {
+              id: 'form'
+          })
+          .append(
+            $('<fieldset>', {
+                id: 'creep_general'
+            })
+            .append(
+                $('<legend>', { })
+                .html("General")
+            )
+            .append(
+                $('<input>', {
+                    type: 'checkbox',
+                    id: 'creep_gen_show',
+                    name: 'creep_gen_show',
+                    value: 'checked',
+                })
+            )
+            .append(
+                $('<label>', {
+                    for: 'creep_gen_show',
+                })
+                .html('Show only creeps that break the threshold distance.')
+            )
+          )
+          .append(
+            $('<br>', { })
+          )
+          .append(
+              $('<fieldset>', {
+                  id: 'creep_thresh_dist',
+              })
+              .append(
+                  $('<legend>', { })
+                  .html("Threshold Distance")
+              )
+              .append(
+                $('<label>', {
+                    for: 'creep_thresh_dist_val',
+                })
+                .html('Treat devices with distance over this value as creeps:')
+              )
+              .append(
+                $('<input>', {
+                  type: 'text',
+                  name: 'creep_thresh_dist_val',
+                  id: 'creep_thresh_dist_val'
+                })
+              )
+          )
+          .append(
+            $('<br>', { })
+          )
+          .append(
+              $('<fieldset>', {
+                  id: 'creep_thresh_unit'
+              })
+              .append(
+                  $('<legend>', { })
+                  .html("Threshold Unit")
+              )
+              .append(
+                  $('<input>', {
+                      type: 'radio',
+                      id: 'creep_thresh_unit_km',
+                      name: 'creep_thresh_unit_val',
+                      value: 'km',
+                  })
+              )
+              .append(
+                  $('<label>', {
+                      for: 'creep_thresh_unit_km',
+                  })
+                  .html('Kilometers (km)')
+              )
+              .append(
+                  $('<input>', {
+                      type: 'radio',
+                      id: 'creep_thresh_unit_mile',
+                      name: 'creep_thresh_unit_val',
+                      value: 'mile',
+                  })
+              )
+              .append(
+                  $('<label>', {
+                      for: 'creep_thresh_unit_mile',
+                  })
+                  .html('Miles (mi)')
+              )
+              .append(
+                $('<input>', {
+                    type: 'radio',
+                    id: 'creep_thresh_unit_m',
+                    name: 'creep_thresh_unit_val',
+                    value: 'meter',
+                })
+              )
+              .append(
+                  $('<label>', {
+                      for: 'creep_thresh_unit_m',
+                  })
+                  .html('Meters (m)')
+              )
+              .append(
+                $('<input>', {
+                    type: 'radio',
+                    id: 'creep_thresh_unit_nmi',
+                    name: 'creep_thresh_unit_val',
+                    value: 'nmi',
+                })
+              )
+              .append(
+                  $('<label>', {
+                      for: 'creep_thresh_unit_nmi',
+                  })
+                  .html('Nautical Miles (nmi)')
+              )
+          )
+          .append(
+            $('<br>', { })
+          )
+          .append(
+            $('<p>')
+            .html('Note: Refresh the Kismet Web UI for changes to take effect.')
+          )
+      );
+
+      $('#form', elem).on('change', function() {
+          kismet_ui_settings.SettingsModified();
+      });
+
+      $('#creep_thresh_dist_val').val(kismet.getStorage('kismet.creepdetector.thresh_dist', DISTANCE_THRESHOLD));
+
+      var thresh_unit = kismet.getStorage('kismet.creepdetector.thresh_unit', DISTANCE_UNIT);
+      if (thresh_unit === 'km') {
+        $('#creep_thresh_unit_km', elem).attr('checked', 'checked');
+      } else if (thresh_unit === 'mile') {
+        $('#creep_thresh_unit_mi', elem).attr('checked', 'checked');
+      } else if (thresh_unit === 'meter') {
+        $('#creep_thresh_unit_m', elem).attr('checked', 'checked');
+      } else if (thresh_unit === 'nmi') {
+        $('#creep_thresh_unit_nmi', elem).attr('checked', 'checked');
+      } else {
+        $('#creep_thresh_unit_mi', elem).attr('checked', 'checked');
+      }
+
+      if (kismet.getStorage('kismet.creepdetector.show_only_creeps') === 'checked') {
+          $('#creep_gen_show', elem).attr('checked', 'checked');
+      } else {
+          $('#creep_gen_show', elem).removeAttr('checked');
+      }
+
+      $('#creep_thresh_dist', elem).controlgroup();
+      $('#creep_thresh_unit', elem).controlgroup();
+      $('#creep_general', elem).controlgroup();
+
+  },
+  save: function(elem) {
+      var thresh_dist = $("input[name='creep_thresh_dist_val']", elem).val();
+      kismet.putStorage('kismet.creepdetector.thresh_dist', thresh_dist);
+
+      var thresh_unit = $("input[name='creep_thresh_unit_val']:checked", elem).val();
+      kismet.putStorage('kismet.creepdetector.thresh_unit', thresh_unit);
+
+      if ($("#creep_gen_show").is(':checked')) {
+        var show_only_creeps = 'checked';
+      } else {
+        var show_only_creeps = '';
+      }
+      kismet.putStorage('kismet.creepdetector.show_only_creeps', show_only_creeps);
+
+      return true;
+  },
+}); // end AddSettingsPane
